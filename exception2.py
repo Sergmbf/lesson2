6b3325c95e576bf32bcef8a8d73ab358c82e0574
Sergey Petrov <serg4309@yandex.ru>
2020-02-22 23:32:01 +0300

    (Programm ok)

--- exception2.py
+++ exception2.py
@@ -13,14 +13,15 @@
 """
 
 def get_summ(num_one, num_two):
-    """
-    Замените pass на ваш код
-    """
-    pass
-    
-if __name__ == "__main__":
-    print(get_summ(2, 2))
-    print(get_summ(3, "3"))
-    print(get_summ("4", "4"))
-    print(get_summ("five", 5))
-    print(get_summ("six", "шесть"))
+
+  try:
+    a = (int(num_one)+int(num_two))
+    print(a)
+  except ValueError: 
+    print('ошибка ввода числа')
+
+print(get_summ(2, 2))
+print(get_summ(3, "3"))
+print(get_summ("4", "4"))
+print(get_summ("five", 5))
+print(get_summ("six", "шесть"))
