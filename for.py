7cb71bbd469382e1df892d5ed042497901e80108
Sergey Petrov <serg4309@yandex.ru>
2020-02-22 08:46:54 +0300

    Commit 3

--- for.py
+++ for.py
@@ -9,13 +9,30 @@
 * Посчитать и вывести средний балл по всей школе.
 * Посчитать и вывести средний балл по каждому классу.
 """
+list_scores_school = [
+  {'school_class':'4a','scores':[3,4,5,5,3,4,3]},
+  {'school_class':'3b','scores':[5,5,5,5,5,5,5]},
+  {'school_class':'7a','scores':[3,4,5,4,3,3,3]},
+  {'school_class':'4b','scores':[5,5,5,5,5,4,5]},
+  {'school_class':'5a','scores':[5,5,5,5,4,4,3]}
+  ]
 
-def main():
+def scores_school():
     """
     Эта функция вызывается автоматически при запуске скрипта в консоли
     В ней надо заменить pass на ваш код
     """
-    pass
-    
-if __name__ == "__main__":
-    main()
+    # average_score_class('school_class') = dict()
+    sum_scores_school = 0
+    for j in ['school_class']:
+        sum_scores_school += sum_scores_school
+        sum_scores_class('school_class') = 0 
+        for i in ['scores']:
+            sum_scores_class('school_class') += sum_scores_class('school_class')
+        average_scores_class('school_class') = sum_scores_class('school_class')/ int(len('scores'))
+        return average_scores_class('school_class')
+    average_scores_school = sum_scores_school/ int(len('school_class'))
+    return average_scores_school
+
+print('average_score_class =', average_score_class('4a'))
+print('average_scores_school =', average_scores_school )
